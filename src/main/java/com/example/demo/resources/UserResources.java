9558713e73812dd22046f878f58698b8b1714de9
Wagner perfeito <wagnerperfeito@wpcoms.com.br>
2024-08-28 17:25:22 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/example/demo/resources/UserResources.java
+++ src/main/java/com/example/demo/resources/UserResources.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.demo.domain.Post;
 import com.example.demo.domain.User;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.services.UserService;
@@ -58,5 +59,10 @@ public class UserResources {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+		}
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 		}
 }
